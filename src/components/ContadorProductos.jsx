f9e19e87def28553519511a197c4c51a1fba603d
Karen Ramirez <karen_ramirez82181@elpoli.edu.co>
2023-04-17 14:28:20 -0500

    Se corrige logica de la cantidad del input de CantidadElementoCarrito

--- src/components/ContadorProductos.jsx
+++ src/components/ContadorProductos.jsx
@@ -4,26 +4,11 @@ import { useEffect } from "react";
 const ContadorProductos = ({productos,setProductos, elementosCarrito,setElementosCarrito,elementoCarrito,inputCantidad, setInputCantidad}) => {
   const cantidadProductos= null;
 
-  //cantidadProductos.onchange=function(event){if (event.inputType === "increment" || event.inputType === "decrement") {
-    //console.log("Se hizo clic en la flecha hacia " + (event.inputType === "increment" ? "arriba" : "abajo"));
-    // Llama a tu función aquí
-  //}}
-
   const handlerInput = ({target})=>{
-    //setInputCantidad(cantidadProductos);
-    console.log("target"+target.value)
+    console.log("target"+target.value);
+    const newElementos = elementosCarrito.map(e => e.id === elementoCarrito.id ? {...e,cantidad:parseInt(target.value)}:e);
+    setElementosCarrito(newElementos);
   };
-
-  /*const manejarCambio = (evento) => {
-    console.log('Function' + evento.nativeEvent.inputType+)
-    if (evento.inputType === "increment" || evento.inputType === "decrement") {
-      console.log(`Se hizo clic en la flecha hacia ${evento.inputType === "increment" ? "arriba" : "abajo"}`);
-      // Llama a tu función aquí
-    }
-  };
-  useEffect(() => {
-    setInputCantidad(elementoCarrito.cantidad)
-  });*/
   return (
     <div>
       <input
