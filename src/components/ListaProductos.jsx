fdca330a7290f4960df4e8bd19f3ac4bf5ae3b02
Jhoan Palomino <jhoan_palomino82181@elpoli.edu.co>
2023-04-18 16:34:17 -0500

    Inicio de estilos lista productos

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Producto from './Producto'
-
+import styles from './ListaProductos.module.css'
 
 const ListaProductos = ({productos,setProductos, elementosCarrito,setElementosCarrito}) => {
     
   return (
-    <div>
+    <div className={styles.gridContainer}>
       {
         productos.map(producto => (
           <div>
