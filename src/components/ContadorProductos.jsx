b2efedc52c8e6a09c76731ccee9e8de35a2a1ff0
Karen Ramirez <karen_ramirez82181@elpoli.edu.co>
2023-04-18 15:19:29 -0500

    Se organiza problema de resta en products y doble resta en productos

--- src/components/ContadorProductos.jsx
+++ src/components/ContadorProductos.jsx
@@ -5,13 +5,13 @@ const ContadorProductos = ({productos,setProductos, elementosCarrito,setElemento
 
   const handlerInput = ({target})=>{
     const movimiento = parseInt(target.value)<elementoCarrito.cantidad?'R':'S';
+    
+    const newElementos = elementosCarrito.map(e => e.id === elementoCarrito.id ? {...e,cantidad:parseInt(target.value)}:e);
+    setElementosCarrito(newElementos);
 
     const newProductos = productos.map(
       p => p.id === elementoCarrito.id ? {...p, cantidad:movimiento==='S'?p.cantidad-1:p.cantidad+1} : p);
     setProductos(newProductos);
-    
-    const newElementos = elementosCarrito.map(e => e.id === elementoCarrito.id ? {...e,cantidad:parseInt(target.value)}:e);
-    setElementosCarrito(newElementos);
   };
   return (
     <div>
