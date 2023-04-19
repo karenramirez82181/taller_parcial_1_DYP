3996fcda4204564905615a60245636af2ec55dc3
Jhoan Palomino <jhoan_palomino82181@elpoli.edu.co>
2023-04-19 00:04:48 -0500

    Completada la logica de los input number en Summary y Carrito

--- src/components/ContadorProductos.jsx
+++ src/components/ContadorProductos.jsx
@@ -4,14 +4,44 @@ import React from 'react'
 const ContadorProductos = ({products,productos,setProductos, elementosCarrito,setElementosCarrito,elementoCarrito}) => {
 
   const handlerInput = ({target})=>{
-    const movimiento = parseInt(target.value) < elementoCarrito.cantidad ? 'Resta' : 'Suma';
     
-    const newElementos = elementosCarrito.map(e => e.id === elementoCarrito.id ? {...e,cantidad:parseInt(target.value)}:e);
-    setElementosCarrito(newElementos);
+    //Si no es vacio ni negativo entra a ingresar valores
+    if(target.value != '' && !(target.value.startsWith('-')))
+    {
+      //Si el valor ingresado es menor al stock del articulo entra a ingresar valores
+      if(parseInt(target.value) <= products.filter(pr => pr.id === elementoCarrito.id).map(pr => pr.cantidad))
+      {
+        const movimiento = parseInt(target.value) < elementoCarrito.cantidad ? 'Resta' : 'Suma';
+      
+        const newElementos = elementosCarrito.map(e => e.id === elementoCarrito.id ? {...e,cantidad:parseInt(target.value)}:e);
+        setElementosCarrito(newElementos);
+  
+        const newProductos = productos.map(
+          p => p.id === elementoCarrito.id ? {...p, cantidad:movimiento === 'Suma' ? p.cantidad - 1 : p.cantidad + 1} : p);
+        setProductos(newProductos);
+      }
+      else//Sino, procede a colocar la cantidad maxima del stock por producto
+      {
+        const newElementos = elementosCarrito.map(
+          e => e.id === elementoCarrito.id ? {...e,cantidad:products.filter(pr => pr.id === elementoCarrito.id).map(pr => pr.cantidad)[0]}:e);
+        setElementosCarrito(newElementos);
 
-    const newProductos = productos.map(
-      p => p.id === elementoCarrito.id ? {...p, cantidad:movimiento === 'Suma' ? p.cantidad - 1 : p.cantidad + 1} : p);
-    setProductos(newProductos);
+        const newProductos = productos.map(p => p.id === elementoCarrito.id ? {...p, cantidad:0} : p);
+        setProductos(newProductos);
+      }
+    }
+    else//Sino se coloca el valor 0 por defecto
+    {
+      const newElementos = elementosCarrito.map(e => e.id === elementoCarrito.id ? {...e,cantidad:0}:e);
+      setElementosCarrito(newElementos);
+
+      const newProductos = productos.map(
+        p => p.id === elementoCarrito.id ? {...p, cantidad:products.filter(pr => pr.id === elementoCarrito.id).map(pr => pr.cantidad)[0]} : p);
+        setProductos(newProductos);
+    }
+    
+    
+
   };
   
   return (
